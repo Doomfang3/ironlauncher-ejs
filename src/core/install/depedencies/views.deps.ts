30350f4e26d3d494eae0d415d88322eacb3cf6c6
Doomfang3 <saubinmathieu@gmail.com>
2022-05-18 14:31:34 +0200

    Replace Handlebars by Ejs

--- src/core/install/depedencies/views.deps.ts
+++ src/core/install/depedencies/views.deps.ts
@@ -1,8 +1,10 @@
-import { baseCookieAuth, baseExpress } from "./base.deps";
+import { baseCookieAuth, baseExpress } from './base.deps'
 
-export const VIEWS_EXPRESS_BASE_DEPS = [...baseExpress, `hbs`, `serve-favicon`];
+export const VIEWS_EXPRESS_BASE_DEPS = [
+  ...baseExpress,
+  `ejs`,
+  `express-ejs-layouts`,
+  `serve-favicon`,
+]
 
-export const VIEWS_EXPRESS_AUTH_DEPTS = [
-  ...VIEWS_EXPRESS_BASE_DEPS,
-  ...baseCookieAuth,
-];
+export const VIEWS_EXPRESS_AUTH_DEPTS = [...VIEWS_EXPRESS_BASE_DEPS, ...baseCookieAuth]
